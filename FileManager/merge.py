1316a7b9bdc5baebe4ea2002b2ba6dc82ecc44e1
sonbyeongho <sbh9937@naver.com>
2017-05-22 18:19:52 +0900

    merge bug fix

--- FileManager/merge.py
+++ FileManager/merge.py
@@ -4,16 +4,17 @@ import shutil
 import glob
 
 class mergeFile():
-    def merging(filename,extension):
-        outfilename = filename+'.'+extension
-        #print(filename)
-        globfile = outfilename+'.'+'*'
-        with open(outfilename,'wb') as outfile:
-            for filename in glob.glob('test.*'):
-                if filename == outfilename:
-                    continue
-                with open(filename,'rb') as readfile:
-                    shutil.copyfileobj(readfile,outfile)
+    def merging(inputfilename,extension):
+        outfilename = inputfilename+'.'+extension
 
+        with open(outfilename,'wb') as outfile:
+            counter = 1
+            while True:
+                partfilename = inputfilename+'.'+str(counter)
+                if (os.path.isfile(partfilename)==False):
+                    break
+                with open(partfilename,'rb') as readfile:
+                    shutil.copyfileobj(readfile,outfile)
+                counter += 1
 
 
